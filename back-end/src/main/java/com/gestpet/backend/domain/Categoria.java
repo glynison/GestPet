27522520256a0d6037eafc6515d2ec800c24276e
thiagomurray <89616600+thiagomurray@users.noreply.github.com>
2022-05-30 22:40:21 -0300

    Injeção de dependências STS

--- back-end/src/main/java/com/gestpet/backend/domain/Categoria.java
+++ back-end/src/main/java/com/gestpet/backend/domain/Categoria.java
@@ -6,11 +6,12 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
-import org.springframework.data.annotation.Id;
+
 
 import lombok.Data;
 
